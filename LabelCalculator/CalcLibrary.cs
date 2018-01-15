1c7780de54d73e9b3d69d06108fe159c0d0da6bc
Brad Lundy <crusin67@hotmail.com>
2018-01-14 19:55:03 -0800

    Added calculations to the pd off set button

--- LabelCalculator/CalcLibrary.cs
+++ LabelCalculator/CalcLibrary.cs
@@ -50,12 +50,12 @@ namespace LabelCalculator
             return eighthPitch.ToString();
         }
 
-        public int LabelTotals(double overs, double extra)
+        public string LabelTotals(double overs, double extra)
         {
             double overPercentage = overs;
             double embossOrHotStampExtra = extra * fiftyFeetImages;
-            double labelTotal = ((labelQuantity / labelImages) * overPercentage + fiftyFeetImages + embossOrHotStampExtra);
-            return (int)labelTotal + 1;
+            int labelTotal = (int)((labelQuantity / labelImages) * overPercentage + fiftyFeetImages + embossOrHotStampExtra) + 1;
+            return labelTotal.ToString();
         }
 
         public string TailCalculation()
@@ -78,6 +78,7 @@ namespace LabelCalculator
 
         public string FiftyFootCalculation()
         {
+            CalcuateFiftyFeetImages();
             int fiftyFoot = (int)fiftyFeetImages;
             return fiftyFoot.ToString();
         }
@@ -92,6 +93,186 @@ namespace LabelCalculator
         {
             int threeHundredFoot = (int)(fiftyFeetImages * 6);
             return threeHundredFoot.ToString();
+        }
+
+        public string White1R1PDOffSet(string w1R1, double resolution)
+        {
+            double newW1R1 = (double.Parse(w1R1) * resolution) / 360;
+            return newW1R1.ToString("F");
+        }
+
+        public string White1R2PDOffSet(string w1R2, double resolution)
+        {
+            double newW1R2 = (double.Parse(w1R2) * resolution) / 360;
+            return newW1R2.ToString("F");
+        }
+
+        public string White2R1PDOffSet(string w2R1, double resolution)
+        {
+            double newW2R1 = (double.Parse(w2R1) * resolution) / 360;
+            return newW2R1.ToString("F");
+        }
+
+        public string White2R2PDOffSet(string w2R2, double resolution)
+        {
+            double newW2R2 = (double.Parse(w2R2) * resolution) / 360;
+            return newW2R2.ToString("F");
+        }
+
+        public string White3R1PDOffSet(string w3R1, double resolution)
+        {
+            double newW3R1 = (double.Parse(w3R1) * resolution) / 360;
+            return newW3R1.ToString("F");
+        }
+
+        public string White3R2PDOffSet(string w3R2, double resolution)
+        {
+            double newW3R2 = (double.Parse(w3R2) * resolution) / 360;
+            return newW3R2.ToString("F");
+        }
+
+        public string Cyan1R1PDOffSet(string c1R1, double resolution)
+        {
+            double newC1R1 = (double.Parse(c1R1) * resolution) / 360;
+            return newC1R1.ToString("F");
+        }
+
+        public string Cyan1R2PDOffSet(string c1R2, double resolution)
+        {
+            double newC1R2 = (double.Parse(c1R2) * resolution) / 360;
+            return newC1R2.ToString("F");
+        }
+
+        public string Cyan2R1PDOffSet(string c2R1, double resolution)
+        {
+            double newC2R1 = (double.Parse(c2R1) * resolution) / 360;
+            return newC2R1.ToString("F");
+        }
+
+        public string Cyan2R2PDOffSet(string c2R2, double resolution)
+        {
+            double newC2R2 = (double.Parse(c2R2) * resolution) / 360;
+            return newC2R2.ToString("F");
+        }
+
+        public string Cyan3R1PDOffSet(string c3R1, double resolution)
+        {
+            double newC3R1 = (double.Parse(c3R1) * resolution) / 360;
+            return newC3R1.ToString("F");
+        }
+
+        public string Cyan3R2PDOffSet(string c3R2, double resolution)
+        {
+            double newC3R2 = (double.Parse(c3R2) * resolution) / 360;
+            return newC3R2.ToString("F");
+        }
+
+        public string Magenta1R1PDOffSet(string m1R1, double resolution)
+        {
+            double newM1R1 = (double.Parse(m1R1) * resolution) / 360;
+            return newM1R1.ToString("F");
+        }
+
+        public string Magenta1R2PDOffSet(string m1R2, double resolution)
+        {
+            double newM1R2 = (double.Parse(m1R2) * resolution) / 360;
+            return newM1R2.ToString("F");
+        }
+
+        public string Magenta2R1PDOffSet(string m2R1, double resolution)
+        {
+            double newM2R1 = (double.Parse(m2R1) * resolution) / 360;
+            return newM2R1.ToString("F");
+        }
+
+        public string Magenta2R2PDOffSet(string m2R2, double resolution)
+        {
+            double newM2R2 = (double.Parse(m2R2) * resolution) / 360;
+            return newM2R2.ToString("F");
+        }
+
+        public string Magenta3R1PDOffSet(string m3R1, double resolution)
+        {
+            double newM3R1 = (double.Parse(m3R1) * resolution) / 360;
+            return newM3R1.ToString("F");
+        }
+
+        public string Magenta3R2PDOffSet(string m3R2, double resolution)
+        {
+            double newM3R2 = (double.Parse(m3R2) * resolution) / 360;
+            return newM3R2.ToString("F");
+        }
+
+        public string Yellow1R1PDOffSet(string y1R1, double resolution)
+        {
+            double newY1R1 = (double.Parse(y1R1) * resolution) / 360;
+            return newY1R1.ToString("F");
+        }
+
+        public string Yellow1R2PDOffSet(string y1R2, double resolution)
+        {
+            double newY1R2 = (double.Parse(y1R2) * resolution) / 360;
+            return newY1R2.ToString("F");
+        }
+
+        public string Yellow2R1PDOffSet(string y2R1, double resolution)
+        {
+            double newY2R1 = (double.Parse(y2R1) * resolution) / 360;
+            return newY2R1.ToString("F");
+        }
+
+        public string Yellow2R2PDOffSet(string y2R2, double resolution)
+        {
+            double newY2R2 = (double.Parse(y2R2) * resolution) / 360;
+            return newY2R2.ToString("F");
+        }
+
+        public string Yellow3R1PDOffSet(string y3R1, double resolution)
+        {
+            double newY3R1 = (double.Parse(y3R1) * resolution) / 360;
+            return newY3R1.ToString("F");
+        }
+
+        public string Yellow3R2PDOffSet(string y3R2, double resolution)
+        {
+            double newY3R2 = (double.Parse(y3R2) * resolution) / 360;
+            return newY3R2.ToString("F");
+        }
+
+        public string Black1R1PDOffSet(string k1R1, double resolution)
+        {
+            double newK1R1 = (double.Parse(k1R1) * resolution) / 360;
+            return newK1R1.ToString("F");
+        }
+
+        public string Black1R2PDOffSet(string k1R2, double resolution)
+        {
+            double newK1R2 = (double.Parse(k1R2) * resolution) / 360;
+            return newK1R2.ToString("F");
+        }
+
+        public string Black2R1PDOffSet(string k2R1, double resolution)
+        {
+            double newK2R1 = (double.Parse(k2R1) * resolution) / 360;
+            return newK2R1.ToString("F");
+        }
+
+        public string Black2R2PDOffSet(string k2R2, double resolution)
+        {
+            double newK2R2 = (double.Parse(k2R2) * resolution) / 360;
+            return newK2R2.ToString("F");
+        }
+
+        public string Black3R1PDOffSet(string k3R1, double resolution)
+        {
+            double newK3R1 = (double.Parse(k3R1) * resolution) / 360;
+            return newK3R1.ToString("F");
+        }
+
+        public string Black3R2PDOffSet(string k3R2, double resolution)
+        {
+            double newK3R2 = (double.Parse(k3R2) * resolution) / 360;
+            return newK3R2.ToString("F");
         }
     }
 }
--- LabelCalculator/MainWindow.xaml.cs
+++ LabelCalculator/MainWindow.xaml.cs
@@ -100,22 +100,24 @@ namespace LabelCalculator
         {
             try
             {
+                string labelsToPrint = calc.LabelTotals(percentage, extraFootage);
                 calc.LabelInfo(LabelQuanityTextBox.Text, PixelLengthTextBox.Text, LabelsPerImageTextBox.Text, EyeMarksTextBox.Text);
-                calc.LabelTotals(percentage, extraFootage);
+
+                TotalLabelsTextBox.Text = labelsToPrint;
                 TailTextBox.Text = calc.TailCalculation();
                 LeaderTextBox.Text = calc.LeaderCalculation();
-                itemBlankTextBox.Text = calc.ItemBlankCalculation();
+                ItemBlankTextBox.Text = calc.ItemBlankCalculation();
                 FiftyFeetTextBox.Text = calc.FiftyFootCalculation();
-                OneHundredFeetTextBox.Text = calc.FiftyFootCalculation();
+                OneHundredFeetTextBox.Text = calc.OneHundredFootCalculation();
                 ThreeHundredFeetTextBox.Text = calc.ThreeHundredFootCalcation();
                 OneEighthPitchTextBox.Text = calc.EighthPitchCalculation();
                 ThirtyTwoPitchTextBox.Text = calc.ThirtyTwoPitchCalculation();
+
             }
             catch
             {
                 MessageBox.Show("You did not enter in a correct number", "Warning", MessageBoxButton.OK, MessageBoxImage.Error);
             }
-
         }
 
         private void ClearListButton_Click(object sender, RoutedEventArgs e)
@@ -146,18 +148,15 @@ namespace LabelCalculator
             }
         }
 
-
-
-        private void PDOffSetButton_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
         private void CheckBoxEmboss_Checked(object sender, RoutedEventArgs e)
         {
             if (CheckBoxEmboss.IsChecked == true)
             {
                 extraFootage = 2;
+            }
+            else
+            {
+                extraFootage = 0;
             }
         }
 
@@ -166,6 +165,10 @@ namespace LabelCalculator
             if (CheckBoxHotStamp.IsChecked == true)
             {
                 extraFootage = 4;
+            }
+            else
+            {
+                extraFootage = 0;
             }
         }
 
@@ -230,6 +233,99 @@ namespace LabelCalculator
             var comboBox = sender as ComboBox;
             comboBox.ItemsSource = resolution;
             comboBox.SelectedIndex = 0;
+        }
+
+        private void PDOffSetButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                switch (ComboBoxResolution.SelectedIndex)
+                {
+                    case 1:
+                        double resHigh = 270;
+                        NewPDOffSetResolutions(resHigh);
+                        break;
+                    case 2:
+                        double resMed = 225;
+                        NewPDOffSetResolutions(resMed);
+                        break;
+                    case 3:
+                        double resLow = 180;
+                        NewPDOffSetResolutions(resLow);
+                        break;
+                    default:
+                        break;
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Not all values were enter in correctly.", "Warning", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        public void NewPDOffSetResolutions(double resolution)
+        {
+            string w1r1 = calc.White1R1PDOffSet(W1R1TextBox.Text, resolution);
+            string w1r2 = calc.White1R1PDOffSet(W1R2TextBox.Text, resolution);
+            string w2r1 = calc.White1R1PDOffSet(W2R1TextBox.Text, resolution);
+            string w2r2 = calc.White1R1PDOffSet(W2R2TextBox.Text, resolution);
+            string w3r1 = calc.White1R1PDOffSet(W3R1TextBox.Text, resolution);
+            string w3r2 = calc.White1R1PDOffSet(W3R2TextBox.Text, resolution);
+            string c1r1 = calc.White1R1PDOffSet(C1R1TextBox.Text, resolution);
+            string c1r2 = calc.White1R1PDOffSet(C1R2TextBox.Text, resolution);
+            string c2r1 = calc.White1R1PDOffSet(C2R1TextBox.Text, resolution);
+            string c2r2 = calc.White1R1PDOffSet(C2R2TextBox.Text, resolution);
+            string c3r1 = calc.White1R1PDOffSet(C3R1TextBox.Text, resolution);
+            string c3r2 = calc.White1R1PDOffSet(C3R2TextBox.Text, resolution);
+            string m1r1 = calc.White1R1PDOffSet(M1R1TextBox.Text, resolution);
+            string m1r2 = calc.White1R1PDOffSet(M1R2TextBox.Text, resolution);
+            string m2r1 = calc.White1R1PDOffSet(M2R1TextBox.Text, resolution);
+            string m2r2 = calc.White1R1PDOffSet(M2R2TextBox.Text, resolution);
+            string m3r1 = calc.White1R1PDOffSet(M3R1TextBox.Text, resolution);
+            string m3r2 = calc.White1R1PDOffSet(M3R2TextBox.Text, resolution);
+            string y1r1 = calc.White1R1PDOffSet(Y1R1TextBox.Text, resolution);
+            string y1r2 = calc.White1R1PDOffSet(Y1R2TextBox.Text, resolution);
+            string y2r1 = calc.White1R1PDOffSet(Y2R1TextBox.Text, resolution);
+            string y2r2 = calc.White1R1PDOffSet(Y2R2TextBox.Text, resolution);
+            string y3r1 = calc.White1R1PDOffSet(Y3R1TextBox.Text, resolution);
+            string y3r2 = calc.White1R1PDOffSet(Y3R2TextBox.Text, resolution);
+            string k1r1 = calc.White1R1PDOffSet(K1R1TextBox.Text, resolution);
+            string k1r2 = calc.White1R1PDOffSet(K1R2TextBox.Text, resolution);
+            string k2r1 = calc.White1R1PDOffSet(K2R1TextBox.Text, resolution);
+            string k2r2 = calc.White1R1PDOffSet(K2R2TextBox.Text, resolution);
+            string k3r1 = calc.White1R1PDOffSet(K3R1TextBox.Text, resolution);
+            string k3r2 = calc.White1R1PDOffSet(K3R2TextBox.Text, resolution);
+
+            NewResolutionW1R1TextBox.Text = w1r1;
+            NewResolutionW1R2TextBox.Text = w1r2;
+            NewResolutionW2R1TextBox.Text = w2r1;
+            NewResolutionW2R2TextBox.Text = w2r2;
+            NewResolutionW3R1TextBox.Text = w3r1;
+            NewResolutionW3R2TextBox.Text = w3r2;
+            NewResolutionC1R1TextBox.Text = c1r1;
+            NewResolutionC1R2TextBox.Text = c1r2;
+            NewResolutionC2R1TextBox.Text = c2r1;
+            NewResolutionC2R2TextBox.Text = c2r2;
+            NewResolutionC3R1TextBox.Text = c3r1;
+            NewResolutionC3R2TextBox.Text = c3r2;
+            NewResolutionM1R1TextBox.Text = m1r1;
+            NewResolutionM1R2TextBox.Text = m1r2;
+            NewResolutionM2R1TextBox.Text = m2r1;
+            NewResolutionM2R2TextBox.Text = m2r2;
+            NewResolutionM3R1TextBox.Text = m3r1;
+            NewResolutionM3R2TextBox.Text = m3r2;
+            NewResolutionY1R1TextBox.Text = y1r1;
+            NewResolutionY1R2TextBox.Text = y1r2;
+            NewResolutionY2R1TextBox.Text = y2r1;
+            NewResolutionY2R2TextBox.Text = y2r2;
+            NewResolutionY3R1TextBox.Text = y3r1;
+            NewResolutionY3R2TextBox.Text = y3r2;
+            NewResolutionK1R1TextBox.Text = k1r1;
+            NewResolutionK1R2TextBox.Text = k1r2;
+            NewResolutionK2R1TextBox.Text = k2r1;
+            NewResolutionK2R2TextBox.Text = k2r2;
+            NewResolutionK3R1TextBox.Text = k3r1;
+            NewResolutionK3R2TextBox.Text = k3r2;
         }
     }
 }
